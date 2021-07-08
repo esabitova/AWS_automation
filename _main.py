806ce9e9f00a9834e3c6231d730313f3ca8c7cdf
Elena Sabitova <eg.sabi@gmail.com>
2021-07-08 17:02:34 +0200

    refactoring

--- _main.py
+++ _main.py
@@ -1,29 +1,26 @@
 import boto3
 from cfn_tools import load_yaml, dump_yaml
 
-text = open('./sqs/cf_template.yml').read()
+text = open('./cfn/template.yml').read()
 data = load_yaml(text)
-
+# TODO: clean
 print(dump_yaml(data))
 
-client = boto3.client('cloudformation')
-response = client.create_stack(
-    StackName='AWS-challenge-task34',
-    TemplateBody=dump_yaml(data),
-    Capabilities=[
-        'CAPABILITY_AUTO_EXPAND','CAPABILITY_IAM'
-    ]
-)
 
-
-#
-#
-# # Press the green button in the gutter to run the script.
 def createCfn():
-    pass
+    client = boto3.client('cloudformation')
+    response = client.create_stack(
+        StackName='AWS-challenge-task34',
+        TemplateBody=dump_yaml(data),
+        Capabilities=[
+            'CAPABILITY_AUTO_EXPAND', 'CAPABILITY_IAM'
+        ]
+    )
+
+
+# def sendMessage():
+
 
 
 if __name__ == '__main__':
     createCfn()
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,25 +1,23 @@
 import boto3
 from datetime import datetime
-import json
 
-
-# def send_message():
 sqs = boto3.client('sqs')
-queue_url = sqs.get_queue_url(QueueName="ff979bcd-c9c9-4ca8-9c63-0341b190876d")
-# print("sqsURL" + str(queue_url))
+# TODO: hardcoded? think how to get QueueName variable without hardcoding
+queue_url = sqs.get_queue_url(QueueName="SQSQueueForLambda")
+# TODO: clean
+print("sqsURL" + str(queue_url))
+
 date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-# TODO: delete json format for message
-message = {"date": date}
 response = sqs.send_message(
     QueueUrl=queue_url['QueueUrl'],
-    MessageBody=json.dumps(message)
+    MessageBody=date
 )
-print(message)
+# TODO: clean
+print(date)
 
 
 def handler(event, context):
     return {
         'body': event
     }
-
