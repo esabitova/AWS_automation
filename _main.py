e584c72566cbbbe8658c16285c2519e9d8e24c18
Elena Sabitova <eg.sabi@gmail.com>
2021-07-12 18:22:44 +0200

    Cloud Watch function. Little changes in Lambda

--- _main.py
+++ _main.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 import boto3
 from cfn_tools import load_yaml, dump_yaml
+
 
 text = open('./cfn/template.yml').read()
 data = load_yaml(text)
@@ -25,8 +28,32 @@ def invokeLambda():
             InvocationType='Event',
             Payload='{}'
         )
+        print(response)
+
+
+def cloudWatch():
+    client = boto3.client('cloudwatch')
+    count = 0
+    response = client.get_metric_statistics(
+        Namespace='AWS/SQS',
+        MetricName='NumberOfMessagesSent',
+        Dimensions=[
+            {'Name': 'QueueName', 'Value': 'SQSQueueForLambda'}
+        ],
+        StartTime=datetime.now() - timedelta(hours=3),
+        EndTime=datetime.now(),
+        Period=60,
+        Statistics=['Sum'],
+        Unit='Count'
+    )
+
+    for i in response['Datapoints']:
+        count = (i['Sum'])
+
+    return count
 
 
 if __name__ == '__main__':
-    createCfn()
-    invokeLambda()
+    # createCfn()
+    # invokeLambda()
+    cloudWatch()
